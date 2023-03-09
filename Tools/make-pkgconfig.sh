0eb1ce59d73280e397e396113a0f58f7a4716db0
Dimi Racordon <dimitri.racordon@gmail.com>
2023-03-08 23:42:55 -0500

    Debug

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-version=$(llvm-config --version)
 filename=/usr/local/lib/pkgconfig/llvm.pc
+
+version=$(llvm-config --version)
+machine="$(uname -s)"
+case "${machine}" in
+    Linux*)   libs="-L/usr/lib -lc++";;
+    Darwin*)  libs="-lc++";;
+    *)        libs=""
+esac
 
 echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
+echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
 echo Cflags: -I$(llvm-config --includedir) >> $filename
