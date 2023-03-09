c0f90b23f56e6e0f024b9f2ebd1dece29aaa455e
Dimi Racordon <dimitri.racordon@gmail.com>
2023-03-09 00:06:31 -0500

    Debug

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-filename=/usr/local/lib/pkgconfig/llvm.pc
+# filename=/usr/local/lib/pkgconfig/llvm.pc
+filename=koala.pc
+llvmconfig=$(which llvm-config)
 
-version=$(llvm-config --version)
+version=$(${llvmconfig} --version)
 machine="$(uname -s)"
 case "${machine}" in
     Linux*)   libs="-L/usr/lib -lc++";;
@@ -14,5 +16,5 @@ echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
-echo Cflags: -I$(llvm-config --includedir) >> $filename
+echo Libs: ${libs} -L$(${llvmconfig} --libdir --system-libs --libs core analysis) >> $filename
+echo Cflags: -I$(${llvmconfig} --includedir) >> $filename
