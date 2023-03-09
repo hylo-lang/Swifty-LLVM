facb583c77283db8d2e2ea5e41d6f85c187d74ad
Dimi Racordon <dimitri.racordon@gmail.com>
2023-03-09 00:25:08 -0500

    Debug

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-# filename=/usr/local/lib/pkgconfig/llvm.pc
-filename=koala.pc
-llvmconfig=$(which llvm-config)
+filename=/usr/local/lib/pkgconfig/llvm.pc
 
-echo $llvmconfig
-version=$(/home/runner/work/Swifty-LLVM/Swifty-LLVM/llvm/bin/llvm-config --version)
+version=$(llvm-config --version)
 machine="$(uname -s)"
 case "${machine}" in
-    Linux*)   libs="-L/usr/lib -lc++";;
-    Darwin*)  libs="-lc++";;
-    *)        libs=""
+  Linux*)   libs="-L/usr/lib -lc++";;
+  Darwin*)  libs="-lc++";;
+  *)        libs=""
 esac
 
 echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: ${libs} -L$(${llvmconfig} --libdir --system-libs --libs core analysis) >> $filename
-echo Cflags: -I$(${llvmconfig} --includedir) >> $filename
+echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
+echo Cflags: -I$(llvm-config --includedir) >> $filename
