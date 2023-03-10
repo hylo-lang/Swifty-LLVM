bce185c9258ba16b7d67fe1d9454cac0adc6d769
nickpdemarco <nickpdemarco@gmail.com>
2023-03-10 12:57:30 -0500

    Devcontainer working, reproducing llvmc error

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -3,6 +3,9 @@
 version=$(llvm-config --version)
 filename=/usr/local/lib/pkgconfig/llvm.pc
 
+mkdir -p `dirname $filename`
+touch $filename
+
 echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
