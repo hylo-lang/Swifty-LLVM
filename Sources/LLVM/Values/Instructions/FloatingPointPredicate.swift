fc44caa0ac8b6c06387ab285fa5b13a8099ceb60
Dimi Racordon <dimitri.racordon@gmail.com>
2023-03-10 19:06:41 -0500

    Add missing file

---
+++ Sources/LLVM/Values/Instructions/FloatingPointPredicate.swift
@@ -0,0 +1,95 @@
+import llvmc
+
+/// The predicate of an integer comparison.
+///
+/// - Note: Ordered means that neither operand is a QNAN while unordered means that either operand
+///   may be a QNAN.
+public enum FloatingPointPredicate: Hashable {
+
+  /// No comparison; always false.
+  case alwaysFalse
+
+  /// No comparison; always true.
+  case alwaysTrue
+
+  /// Values are ordered and equal.
+  case oeq
+
+  /// Values are ordered and not equal.
+  case one
+
+  /// Values are ordered and LHS is greater than RHS.
+  case ogt
+
+  /// Values are ordered and LHS greater than or equal to RHS.
+  case oge
+
+  /// Values are ordered and LHS is less than RHS.
+  case olt
+
+  /// Values are ordered and LHS is less than or equal to RHS.
+  case ole
+
+  /// Values are ordered (no nans).
+  case ord
+
+  /// Values are unordered or equal.
+  case ueq
+
+  /// Values are unordered or not equal.
+  case une
+
+  /// Values are unordered or LHS is greater than RHS.
+  case ugt
+
+  /// Values are unordered or LHS is greater than or equal to RHS.
+  case uge
+
+  /// Values are unordered or LHS is less than RHS.
+  case ult
+
+  /// Values are unordered or LHS is less than or equal to RHS.
+  case ule
+
+  /// Values are unordered (either nans).
+  case uno
+
+  /// The LLVM identifier of the predicate.
+  internal var llvm: LLVMRealPredicate {
+    switch self {
+    case .alwaysFalse:
+      return LLVMRealPredicateFalse
+    case .alwaysTrue:
+      return LLVMRealPredicateTrue
+    case .oeq:
+      return LLVMRealOEQ
+    case .one:
+      return LLVMRealONE
+    case .ogt:
+      return LLVMRealOGT
+    case .oge:
+      return LLVMRealOGE
+    case .olt:
+      return LLVMRealOLT
+    case .ole:
+      return LLVMRealOLE
+    case .ord:
+      return LLVMRealORD
+    case .ueq:
+      return LLVMRealUEQ
+    case .une:
+      return LLVMRealUNE
+    case .ugt:
+      return LLVMRealUGT
+    case .uge:
+      return LLVMRealUGE
+    case .ult:
+      return LLVMRealULT
+    case .ule:
+      return LLVMRealULE
+    case .uno:
+      return LLVMRealUNO
+    }
+  }
+
+}
