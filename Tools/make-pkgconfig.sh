77bc4a0f9d4f27237e6300f4bd5b141c2cb57b42
nickpdemarco <nickpdemarco@gmail.com>
2023-03-13 14:49:57 -0400

    Explicitly link c++ in make-pkgconfig

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -6,11 +6,19 @@ filename=$1
 mkdir -p `dirname $filename`
 touch $filename
 
+# REVISIT: Why does macos need the standard library explicitly linked, while linux does not?
+# This does not feel like the correct place for this logic.
+machine="$(uname -s)"
+case "${machine}" in
+  Darwin*)  libs="-lc++";;
+  *)        libs=""
+esac
+
 echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
+echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs core analysis) >> $filename
 echo Cflags: -I$(llvm-config --includedir) >> $filename
 
 echo "$filename written:"
