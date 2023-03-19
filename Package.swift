08739250031f755554e092a1250ab1f665678951
Cai BingJun <1945458160@qq.com>
2023-03-19 19:10:20 +0800

    Link LLVM-C only in Windows

--- Package.swift
+++ Package.swift
@@ -1,16 +1,32 @@
 // swift-tools-version: 5.7
 import PackageDescription
 
-let package = Package(
+#if os(Windows)
+  let package = Package(
   name: "Swifty-LLVM",
   products: [
     .library(name: "LLVM", targets: ["LLVM"]),
   ],
   targets: [
     // LLVM API Wrapper.
-    .target(name: "LLVM", dependencies: ["llvmc"]),
+    .target(name: "LLVM", dependencies: ["llvmc"], linkerSettings: [.linkedLibrary("LLVM-C")]),
     .testTarget(name: "LLVMTests", dependencies: ["LLVM"]),
 
     // LLVM's C API
     .systemLibrary(name: "llvmc", pkgConfig: "llvm"),
   ])
+#else
+  let package = Package(
+    name: "Swifty-LLVM",
+    products: [
+      .library(name: "LLVM", targets: ["LLVM"]),
+    ],
+    targets: [
+      // LLVM API Wrapper.
+      .target(name: "LLVM", dependencies: ["llvmc"]),
+      .testTarget(name: "LLVMTests", dependencies: ["LLVM"]),
+
+      // LLVM's C API
+      .systemLibrary(name: "llvmc", pkgConfig: "llvm"),
+    ])
+#endif
