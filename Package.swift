4221b1e419497f2812c717419e884b558d8f4523
Cai BingJun <1945458160@qq.com>
2023-03-20 20:02:33 +0800

    Clean code

--- Package.swift
+++ Package.swift
@@ -1,32 +1,28 @@
 // swift-tools-version: 5.7
 import PackageDescription
 
+var packageTarget = [Target]()
+
+// LLVM API Wrapper.
 #if os(Windows)
-  let package = Package(
+  packageTarget.append(
+    .target(name: "LLVM", dependencies: ["llvmc"], linkerSettings: [.linkedLibrary("LLVM-C")])
+  )
+#else
+  packageTarget.append(
+    .target(name: "LLVM", dependencies: ["llvmc"])
+  )
+#endif
+
+let package = Package(
   name: "Swifty-LLVM",
   products: [
     .library(name: "LLVM", targets: ["LLVM"]),
   ],
-  targets: [
-    // LLVM API Wrapper.
-    .target(name: "LLVM", dependencies: ["llvmc"], linkerSettings: [.linkedLibrary("LLVM-C")]),
+  targets: packageTarget + [
+    // LLVM API Wrapper Test.
     .testTarget(name: "LLVMTests", dependencies: ["LLVM"]),
 
     // LLVM's C API
     .systemLibrary(name: "llvmc", pkgConfig: "llvm"),
   ])
-#else
-  let package = Package(
-    name: "Swifty-LLVM",
-    products: [
-      .library(name: "LLVM", targets: ["LLVM"]),
-    ],
-    targets: [
-      // LLVM API Wrapper.
-      .target(name: "LLVM", dependencies: ["llvmc"]),
-      .testTarget(name: "LLVMTests", dependencies: ["LLVM"]),
-
-      // LLVM's C API
-      .systemLibrary(name: "llvmc", pkgConfig: "llvm"),
-    ])
-#endif
