c02a7543a705b871d9baa973d3f93fb86e313a42
Dimi Racordon <dimitri.racordon@gmail.com>
2023-04-16 16:06:00 -0400

    Add a initializer to create constant arrays from bytes

--- Sources/LLVM/Values/Constants/ArrayConstant.swift
+++ Sources/LLVM/Values/Constants/ArrayConstant.swift
@@ -20,6 +20,12 @@ public struct ArrayConstant: IRValue, Hashable {
     self.count = values.count
   }
 
+  /// Creates a constant array of `i8` in `module`, filled with the contents of `bytes`.
+  public init<S: Sequence>(bytes: S, in module: inout Module) where S.Element == UInt8 {
+    let i8 = IntegerType(8, in: &module)
+    self.init(of: i8, containing: bytes.map({ i8.constant(UInt64($0)) }), in: &module)
+  }
+
 }
 
 extension ArrayConstant: BidirectionalCollection {
