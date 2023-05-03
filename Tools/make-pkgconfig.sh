0bded41a5b66a258db0a29ac73cdde52b2cd7a12
Dimi Racordon <dimitri.racordon@gmail.com>
2023-05-03 01:04:13 -0400

    Implement standard module optimization passes

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -19,7 +19,7 @@ echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs analysis bitwriter core native target) >> $filename
+echo Libs: ${libs} -L$(llvm-config --libdir --system-libs --libs analysis bitwriter core native passes target) >> $filename
 echo Cflags: -I$(llvm-config --includedir) >> $filename
 
 echo "$filename written:"
