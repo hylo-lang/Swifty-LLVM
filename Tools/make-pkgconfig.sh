38ab765eb65b7ffb60bcc73c6833ea43bc86ba02
Dave Abrahams <dabrahams@adobe.com>
2024-02-27 21:01:33 -0800

    Work around https://github.com/hylo-lang/llvm-build/issues/8
    This finds zstd using pkg-config instead of relying on the RUNPATH embedded in llvm-config.

--- Tools/make-pkgconfig.sh
+++ Tools/make-pkgconfig.sh
@@ -2,6 +2,10 @@
 set -e
 set -o pipefail
 
+# Work around https://github.com/hylo-lang/llvm-build/issues/8
+zstd_dash_L="$(pkg-config --libs-only-L libzstd)"
+export DYLD_LIBRARY_PATH="${zstd_dash_L#-L}:$DYLD_LIBRARY_PATH"
+
 version=$(llvm-config --version)
 filename=$1
 
@@ -21,7 +25,7 @@ echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: -L$(llvm-config --libdir) $(pkg-config --libs-only-L libzstd) ${libs} $(llvm-config --system-libs --libs analysis bitwriter core native passes target) >> $filename
+echo Libs: -L$(llvm-config --libdir) ${zstd_dash_L} ${libs} $(llvm-config --system-libs --libs analysis bitwriter core native passes target) >> $filename
 echo Cflags: -I$(llvm-config --includedir) >> $filename
 
 echo "$filename written:"
