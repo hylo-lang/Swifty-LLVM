521dc79da49a16c9def4bce05880ed8a25781345
Dave Abrahams <dabrahams@adobe.com>
2024-03-01 12:47:25 -0800

    Dev container: smaller docker images (#39)
    Download only the elements needed for the target architecture.
    
    Requires `--network=host` passed to `docker build` if not using
    a devcontainer to build the image.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,8 +5,5 @@ set -o pipefail
 echo "Hylo LLVM build type: $HYLO_LLVM_BUILD_TYPE"
 echo
 
-ln -s /opt/llvm-*-$(uname -m)-*-Debug /opt/llvm-Debug
-ln -s /opt/llvm-*-$(uname -m)-*-MinSizeRel /opt/llvm-MinSizeRel
-
 export PATH="/opt/llvm-$HYLO_LLVM_BUILD_TYPE/bin:$PATH"
-./Tools/make-pkgconfig.sh /usr/local/lib/pkgconfig/llvm.pc
+./Tools/make-pkgconfig.sh /usr/local/lib/pkgconfig/llvm.pc > /dev/null
