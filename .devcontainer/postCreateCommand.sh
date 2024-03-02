21eb3281b83cb764cd7e14274a9642177678e421
Dave Abrahams <dabrahams@adobe.com>
2024-03-01 17:11:05 -0800

    Do away with postCreateCommand.
    Fixes #37.
    
    make-pkgconfig.sh moved (and was symlinked from its original location)
    so that it would be within the docker image's build context and could
    be invoked during the build process.

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -o pipefail
-
-echo "Hylo LLVM build type: $HYLO_LLVM_BUILD_TYPE"
-echo
-
-export PATH="/opt/llvm-$HYLO_LLVM_BUILD_TYPE/bin:$PATH"
-./Tools/make-pkgconfig.sh /usr/local/lib/pkgconfig/llvm.pc > /dev/null
