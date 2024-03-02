a53141bfae81f58b71938c5a21b828f913fd7e8d
Dave Abrahams <dabrahams@adobe.com>
2024-03-02 15:51:08 -0800

    Escape all the things.

--- .devcontainer/make-pkgconfig.sh
+++ .devcontainer/make-pkgconfig.sh
@@ -31,12 +31,20 @@ case "${machine}" in
   *)        libs=""
 esac
 
+libs=()
+for x in -L$(llvm-config --libdir) ${zstd_dash_L} ${libs} $(llvm-config --system-libs --libs analysis bitwriter core native passes target); do
+    libs+=($(printf '%q' "$x"))
+done
+cflags=()
+for x in $(llvm-config --cxxflags); do
+    cflags+=($(printf '%q' "$x"))
+done
 echo Name: LLVM > $filename
 echo Description: Low-level Virtual Machine compiler framework >> $filename
 echo Version: $(echo ${version} | sed 's/\([0-9.]\+\).*/\1/') >> $filename
 echo URL: http://www.llvm.org/ >> $filename
-echo Libs: -L$(llvm-config --libdir) ${zstd_dash_L} ${libs} $(llvm-config --system-libs --libs analysis bitwriter core native passes target) >> $filename
-echo Cflags: -I$(llvm-config --cxxflags) >> $filename
+echo Libs: ${libs[@]} >> $filename
+echo Cflags: ${cflags[@]} >> $filename
 
 echo "$filename written:"
 cat $filename
