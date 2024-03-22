04f0b6f3adb40cb7702082d5944a4f63be755040
Dave Abrahams <dabrahams@adobe.com>
2024-03-22 16:07:50 -0700

    Moves dependency resolution into Hylo's common CMakeModules.

--- cmake/TopLevelDefaults.cmake
+++ cmake/TopLevelDefaults.cmake
@@ -1,5 +1,24 @@
-# commands used to create an easy development environment when
+# Commands used to create an easy development environment when
 # building this project directly (not as a dependency).
 
+# Without this, generated Xcode projects aren't debuggable.
 set(CMAKE_XCODE_GENERATE_SCHEME YES)
-include(SwiftyLLVM_FetchDependencies)
+
+#
+# Hylo-standard dependency resolution.
+#
+include(FetchContent)
+
+block()
+
+  set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
+  FetchContent_Declare(Hylo-CMakeModules
+    GIT_REPOSITORY https://github.com/hylo-lang/CMakeModules.git
+    GIT_TAG        main
+    OVERRIDE_FIND_PACKAGE
+  )
+
+endblock()
+FetchContent_MakeAvailable(Hylo-CMakeModules)
+
+list(PREPEND CMAKE_MODULE_PATH ${hylo-cmakemodules_SOURCE_DIR})
