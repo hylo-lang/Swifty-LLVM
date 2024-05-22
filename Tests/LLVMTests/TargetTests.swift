32d7aa6ea7cb5cf1a89f05b441a128cb631829d2
Dave Abrahams <dabrahams@adobe.com>
2024-05-21 18:47:34 -0700

    Explain the test.

--- Tests/LLVMTests/TargetTests.swift
+++ Tests/LLVMTests/TargetTests.swift
@@ -3,7 +3,9 @@ import XCTest
 
 final class TargetTests: XCTestCase {
 
-  func testHostTargetName() throws {
+  func testHostTargetNameCopyValidity() throws {
+    // Check that a copy of the target name can still be used after the lifetime
+    // of the Target ends.
     let h = try Target.host()
     let n = h.name
     XCTAssertFalse(n.isEmpty)
