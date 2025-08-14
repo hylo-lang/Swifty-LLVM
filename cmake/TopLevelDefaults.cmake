67c486c90c1bf046d938c6e8077314e0153ac0c3
Ambrus Toth <ping@ambrus.dev>
2025-08-14 21:28:22 +0200

    Upgrade language version to 6 and update cmakemodules reference

--- cmake/TopLevelDefaults.cmake
+++ cmake/TopLevelDefaults.cmake
@@ -14,7 +14,7 @@ block()
   set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE NEVER)
   FetchContent_Declare(Hylo-CMakeModules
     GIT_REPOSITORY https://github.com/hylo-lang/CMakeModules.git
-    GIT_TAG        8b9300848f5a9eaa951dc9d9627b586c875cacd9
+    GIT_TAG        e3cbef312609da96fa08bb1e50dd7afa2d9f7d7b
     OVERRIDE_FIND_PACKAGE
   )
 
