396e0fb62eafe6a52d9ace1592e5b739d84b6264
Ambrus Toth <ping@ambrus.dev>
2026-02-18 22:33:48 +0100

    swiftc crash

--- Tests/LLVMTests/Types/FunctionTypeTests.swift
+++ Tests/LLVMTests/Types/FunctionTypeTests.swift
@@ -5,15 +5,15 @@ final class FunctionTypeTests: XCTestCase {
 
   func testDefaultReturnType() {
     var m = Module("foo")
-    let f = m.types[m.functionType(from: [])]
+    let f = m.types[m.functionType(from: ())]
     XCTAssert(f.returnType == m.types[m.void])
   }
 
   func testReturnType() {
     var m = Module("foo")
-    let t = m.types[m.integerType(64)]
-    let f = m.types[m.functionType(from: [], to: AnyType.Identity(m.types.id(for: t)!))]
-    XCTAssert(f.returnType == t)
+    let t = m.i64
+    let f = m.types[m.functionType(from: (), to: t)]
+    XCTAssert(f.returnType == m.types[t])
   }
 
   func testParameters() {
@@ -21,17 +21,17 @@ final class FunctionTypeTests: XCTestCase {
     let t = m.types[m.integerType(64)]
     let u = m.types[m.integerType(32)]
 
-    let f0 = m.types[m.functionType(from: [])]
+    let f0 = m.types[m.functionType(from: ())]
     XCTAssertEqual(f0.parameters.count, 0)
 
-    let te = m.types.id(for: t)!.erased
-    let ue = m.types.id(for: u)!.erased
+    let te = m.types.id(for: t)!
+    let ue = m.types.id(for: u)!
 
-    let f1 = m.types[m.functionType(from: [te])]
+    let f1 = m.types[m.functionType(from: (te))]
     XCTAssertEqual(f1.parameters.count, 1)
     XCTAssert(f1.parameters[0] == t)
 
-    let f2 = m.types[m.functionType(from: [te, ue])]
+    let f2 = m.types[m.functionType(from: (te, ue))]
     XCTAssertEqual(f2.parameters.count, 2)
     XCTAssert(f2.parameters[0] == t)
     XCTAssert(f2.parameters[1] == u)
@@ -39,7 +39,7 @@ final class FunctionTypeTests: XCTestCase {
 
   func testConversion() {
     var m = Module("foo")
-    let t: any IRType = m.types[m.functionType(from: [])]
+    let t: any IRType = m.types[m.functionType(from: ())]
     XCTAssertNotNil(FunctionType(t))
     let u: any IRType = m.types[m.integerType(64)]
     XCTAssertNil(FunctionType(u))
@@ -47,16 +47,14 @@ final class FunctionTypeTests: XCTestCase {
 
   func testEquality() {
     var m = Module("foo")
-    let t = m.types[m.integerType(64)]
-    let u = m.types[m.integerType(32)]
-    let tID = AnyType.Identity(m.types.id(for: t)!)
-    let uID = AnyType.Identity(m.types.id(for: u)!)
+    let t = m.i64
+    let u = m.i32
 
-    let f0 = m.types[m.functionType(from: [tID, uID])]
-    let f1 = m.types[m.functionType(from: [tID, uID])]
+    let f0 = m.types[m.functionType(from: (t, u))]
+    let f1 = m.types[m.functionType(from: (t, u))]
     XCTAssertEqual(f0, f1)
 
-    let f2 = m.types[m.functionType(from: [uID, tID])]
+    let f2 = m.types[m.functionType(from: (u, t))]
     XCTAssertNotEqual(f0, f2)
   }
 
