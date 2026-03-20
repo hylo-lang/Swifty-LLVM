6acec939c27b4482bafd88d6252f9ff04dc858b0
Ambrus Tóth <ping@ambrus.dev>
2026-03-20 21:01:33 +0200

    Add stored properties for common shorthand types
    This fixes some common occurrences of https://github.com/swiftlang/swift/issues/87921

--- Sources/SwiftyLLVM/Types/FloatingPointType.swift
+++ Sources/SwiftyLLVM/Types/FloatingPointType.swift
@@ -13,47 +13,96 @@ public struct FloatingPointType: IRType, Hashable {
 
   /// Returns the 16-bit floating-point type `half` in `module`.
   public static func half(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMHalfTypeInContext(module.context))
+    half(in: .init(module.context))
+  }
+
+  /// Returns the 16-bit floating-point type `half` in `context`.
+  static func half(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMHalfTypeInContext(context.raw))
   }
 
   /// Returns the 16-bit "brain" floating-point type `bfloat` in `module`.
   ///
   /// Represented as truncated IEEE 754 binary32, with 1 sign bit, 8 exponent bits and 7 fraction bits.
   public static func bfloat(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMBFloatTypeInContext(module.context))
+    bfloat(in: .init(module.context))
+  }
+
+  /// Returns the 16-bit "brain" floating-point type `bfloat` in `context`.
+  ///
+  /// Represented as truncated IEEE 754 binary32, with 1 sign bit, 8 exponent bits and 7 fraction bits.
+  static func bfloat(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMBFloatTypeInContext(context.raw))
   }
 
   /// Returns the 32-bit floating-point type `float` in `module`.
   ///
   /// Represented as IEEE 754 binary32.
   public static func float(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMFloatTypeInContext(module.context))
+    float(in: .init(module.context))
+  }
+
+  /// Returns the 32-bit floating-point type `float` in `context`.
+  ///
+  /// Represented as IEEE 754 binary32.
+  static func float(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMFloatTypeInContext(context.raw))
   }
 
   /// Returns the 64-bit floating-point type `double` in `module`.
   ///
   /// Represented as IEEE 754 binary64.
   public static func double(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMDoubleTypeInContext(module.context))
+    double(in: .init(module.context))
+  }
+
+  /// Returns the 64-bit floating-point type `double` in `context`.
+  ///
+  /// Represented as IEEE 754 binary64.
+  static func double(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMDoubleTypeInContext(context.raw))
   }
 
   /// Returns the 80-bit floating-point type `x86_fp80` in `module`.
   ///
   /// Represented as in X87.
   public static func x86_fp80(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMX86FP80TypeInContext(module.context))
+    x86_fp80(in: .init(module.context))
+  }
+
+  /// Returns the 80-bit floating-point type `x86_fp80` in `context`.
+  ///
+  /// Represented as in X87.
+  static func x86_fp80(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMX86FP80TypeInContext(context.raw))
   }
 
   /// Returns the 128-bit floating-point type `fp128` in `module`.
+  ///
+  /// Represented as IEEE 754 binary128.
   public static func fp128(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMFP128TypeInContext(module.context))
+    fp128(in: .init(module.context))
+  }
+
+  /// Returns the 128-bit floating-point type `fp128` in `context`.
+  ///
+  /// Represented as IEEE 754 binary128.
+  static func fp128(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMFP128TypeInContext(context.raw))
   }
 
   /// Returns the 128-bit floating-point type `ppc_fp128` in `module`.
   ///
   /// Represented as the PowerPC double-double format, with two 64-bit IEEE 754 binary64 parts.
   public static func ppc_fp128(in module: inout Module) -> FloatingPointType.UnsafeReference {
-    .init(LLVMPPCFP128TypeInContext(module.context))
+    ppc_fp128(in: .init(module.context))
+  }
+
+  /// Returns the 128-bit floating-point type `ppc_fp128` in `context`.
+  ///
+  /// Represented as the PowerPC double-double format, with two 64-bit IEEE 754 binary64 parts.
+  static func ppc_fp128(in context: ContextRef) -> FloatingPointType.UnsafeReference {
+    .init(LLVMPPCFP128TypeInContext(context.raw))
   }
 
   /// Returns a constant whose LLVM IR type is `self` and whose value is `v`.
