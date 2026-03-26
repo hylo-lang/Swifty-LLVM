26c54e68d5f70d48e41215383153bb9557eea79d
Ambrus Toth <ping@ambrus.dev>
2026-03-27 00:23:15 +0200

    Make MVS ptilities internal

--- Sources/SwiftyLLVM/Utils/MutableValueSemantics.swift
+++ Sources/SwiftyLLVM/Utils/MutableValueSemantics.swift
@@ -1,15 +1,15 @@
 /// Returns the application of `action` on an immutable projection of `value`.
-public func read<T, U>(_ value: T, _ action: (T) throws -> U) rethrows -> U {
+func read<T, U>(_ value: T, _ action: (T) throws -> U) rethrows -> U {
   try action(value)
 }
 
 /// Returns the application of `action` on a mutable projection of `value`.
-public func modify<T, U>(_ value: inout T, _ action: (inout T) throws -> U) rethrows -> U {
+func modify<T, U>(_ value: inout T, _ action: (inout T) throws -> U) rethrows -> U {
   try action(&value)
 }
 
 /// Returns the application of `action` on a mutable projection of `value` as an instance of `T`.
-public func modify<T, U>(
+func modify<T, U>(
   _ value: inout Any, as: T.Type, _ action: (inout T) throws -> U
 ) rethrows -> U {
   var v = value as! T
@@ -18,6 +18,6 @@ public func modify<T, U>(
 }
 
 /// Assigns `value` to the result of applying `transform` on it.
-public func update<T>(_ value: inout T, with transform: (T) throws -> T) rethrows {
+func update<T>(_ value: inout T, with transform: (T) throws -> T) rethrows {
   value = try transform(value)
 }
