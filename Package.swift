ca8f44882d51cde4f4a6ca6d12138a0865d6dc67
Dimi Racordon <dimi.racordon@gmail.com>
2026-05-17 15:12:01 +0200

    Cleanup 'Package.swift'

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:6.2
 
-// BEGIN: Poor person's pkg-config processing, since SPM doesn't
-// fully understand pkg-config files on Windows.
+// MARK: Poor person's pkg-config processing, required to handle pkg-config files on Windows.
+
 import Foundation
 import PackageDescription
 
@@ -14,45 +14,39 @@ import PackageDescription
 /// The text used to separate elements of the PATH environment variable.
 let pathSeparator: Character = osIsWindows ? ";" : ":"
 
-/// Returns the contents of the pkg-config file for `package` if they
-/// can be found in `PKG_CONFIG_PATH`.
+/// Returns the contents of the pkg-config file for `package` if they can be found in
+/// `PKG_CONFIG_PATH`.
 ///
-/// N.B. Does not search the standard locations for the file as
-/// pkg-config would.
-func pseudoPkgConfigText(_ package: String) -> String? {
-  guard let pcp = ProcessInfo.processInfo.environment["PKG_CONFIG_PATH"] else { return nil }
-
-  return pcp.split(separator: pathSeparator)
-    .lazy.compactMap({ try? String(contentsOfFile: "\($0)/\(package).pc", encoding: String.Encoding.utf8) }).first
+/// This function does not search the standard locations for the file as pkg-config would.
+func pkgConfigContents(_ package: String) -> String? {
+  if let p = ProcessInfo.processInfo.environment["PKG_CONFIG_PATH"] {
+    for q in p.split(separator: pathSeparator) {
+      if let s = try? String(contentsOfFile: "\(q)/\(package).pc", encoding: .utf8) { return s }
+    }
+  }
+  return nil
 }
 
-/// Returns the un-quoted, un-escaped elements in the remainder of the
-/// any (logical) lines beginning with `"\(key): "` in pcFileText, the contents
-/// of a pkg-config file.
-func pkgConfigValues(in pcFileText: String, for key: String) -> [String] {
+/// Returns the un-quoted, un-escaped elements in the remainder of the (logical) lines beginning
+/// with `"\(key): "` in `pcContents`, which are the contents of a pkg-config file.
+func pkgConfigValues(in pcContents: String, for key: String) -> [String] {
   let keyPattern = NSRegularExpression.escapedPattern(for: key)
-  let lineHeaders = pcFileText.matches(
-    forRegex: #"(?m)(?<!\\[\r]?[\n])^[ \t]*"# + keyPattern
-      + #"[ \t]*:[ \t]*"#).joined().compactMap { $0 }
+  let lineHeadersPattern = #"(?m)(?<!\\[\r]?[\n])^[ \t]*"# + keyPattern + #"[ \t]*:[ \t]*"#
+  let lineHeaders = pcContents.matches(forRegex: lineHeadersPattern).joined().compactMap { $0 }
 
   var r: [String] = []
-
   for h in lineHeaders {
-    var input = pcFileText[h.endIndex...]
-    var open = false
+    var input = pcContents[h.endIndex...]
+
     func add(_ c: Character) {
-      if !open { r.append("") }
       r[r.count - 1].append(c)
-      open = true
     }
 
-    header:
-      while let c = input.popFirst(), !c.isNewline {
+    header: while let c = input.popFirst(), !c.isNewline {
       switch c {
       case "'", "\"":
         let quote = c
-        quoting:
-          while let c1 = input.popFirst() {
+        quoting: while let c1 = input.popFirst() {
           switch c1 {
           case "\\":
             if let c2 = input.popFirst() { add(c2) }
@@ -63,8 +57,8 @@ func pkgConfigValues(in pcFileText: String, for key: String) -> [String] {
         }
       case "\\": if let c1 = input.popFirst() { add(c1) }
       case _ where c.isNewline: break header
-      case _ where c.isWhitespace: open = false
-      case _: add(c)
+      case _ where c.isWhitespace: break
+      case _: r[r.count - 1].append(c)
       }
     }
   }
@@ -73,58 +67,63 @@ func pkgConfigValues(in pcFileText: String, for key: String) -> [String] {
 
 extension String {
 
-  /// Returns, for each match of the valid regular expression
-  /// `pattern`, the given match `groups` in the same order in which
-  /// they were passed.
+  /// Returns, for each match of the valid regular expression `pattern`, the given match `groups`
+  /// in the same order in which they were passed.
   func matches(forRegex pattern: String, groups: [Int] = [0]) -> [[Substring?]] {
     let r = try! NSRegularExpression(pattern: pattern)
-    return r.matches(
-      in: self,
-      range: NSRange(startIndex..<endIndex, in: self)
-    ).map { m in
-      groups.map { g in
-        Range(m.range(at: g), in: self).map { self[ $0 ] }
-      }
+    return r.matches(in: self, range: NSRange(startIndex..<endIndex, in: self)).map { m in
+      groups.map { g in Range(m.range(at: g), in: self).map { self[ $0 ] } }
     }
   }
 
 }
 
 extension Substring {
+
+  /// Returns `prefix` if `self` is equal to `prefix + suffix`; otherwise, returns `self`.
   func droppingSuffix(_ suffix: String) -> Substring {
     self.hasSuffix(suffix) ? Substring(self.dropLast(suffix.count)) : self
   }
+
 }
-// END: Poor person's pkg-config processing.
-// Used to implement `windowsLinkerSettings()` below.
+
+// MARK: Helpers for getting flags and other settings
 
 /// Returns the linker needed for building on Windows.
 func windowsLinkerSettings() -> [LinkerSetting] {
-  guard let t = pseudoPkgConfigText("llvm") else { return [] }
+  guard let t = pkgConfigContents("llvm") else { return [] }
 
   let libs = pkgConfigValues(in: t, for: "Libs")
-  let linkLibraries = libs.lazy.filter { $0.starts(with: "-l") || $0.first != "-" }.map {
-    let rest = $0.dropFirst($0.first == "-" ? 2 : 0)
-    let afterSlashes = rest.lastIndex(where: { $0 == "/" || $0 == "\\" })
-      .map { rest.index(after: $0) } ?? rest.startIndex
-    return rest[afterSlashes...]
-  }
+  return libs.compactMap { (lib) -> LinkerSetting? in
+    if !lib.starts(with: "-l") && lib.first != "-" { return nil }
 
-  return Array(linkLibraries.map { LinkerSetting.linkedLibrary(String($0.droppingSuffix(".lib"))) })
+    let rest = lib.dropFirst(lib.first == "-" ? 2 : 0)
+    let afterSlashes = rest.lastIndex(where: { $0 == "/" || $0 == "\\" }).map {
+      rest.index(after: $0)
+    } ?? rest.startIndex
+
+    let s = rest[afterSlashes...].droppingSuffix(".lib")
+    return LinkerSetting.linkedLibrary(String(s))
+  }
 }
 
-/// Finds an executable named `name` or `name.exe` on the PATH.
+/// Returns the path to an executable named `name` or `name.exe` if one can be found at one of the
+/// locations in the PATH environment variable; otherwise, returns `nil`.
 func findExecutableOnPath(name: String) -> String? {
   guard let path = ProcessInfo.processInfo.environment["PATH"] else { 
     fatalError("No PATH environment variable found")
   }
   let executableFileName = osIsWindows ? "\(name).exe" : name
 
-  return path.split(separator: pathSeparator)
-    .map { URL(fileURLWithPath: String($0)).appendingPathComponent(executableFileName).path }
-    .first { FileManager.default.isExecutableFile(atPath: $0) }
+  let locations = path.split(separator: pathSeparator)
+  for l in locations {
+    let p = URL(fileURLWithPath: String(l)).appendingPathComponent(executableFileName).path
+    if FileManager.default.isExecutableFile(atPath: p) { return p }
+  }
+  return nil
 }
 
+/// Returns the contents written to the standard output by `executable` ran with `arguments`.
 func readProcessOutput(executable: String, arguments: [String]) -> String {
   let process = Process()
   process.executableURL = URL(fileURLWithPath: executable)
@@ -139,28 +138,30 @@ func readProcessOutput(executable: String, arguments: [String]) -> String {
   return String(data: data, encoding: .utf8)!
 }
 
-/// LLVM requires zstd to be linked dynamically (doesn't come with the installed package).
-/// 
-/// This technique is not reliable on Linux yet because zstd generates wrong pkgconfig file there. 
-/// See https://github.com/facebook/zstd/issues/4488 (TL;DR: libzstd.pc contains a non-architecture specific library directory on Linux).
-/// 
-/// However, it is only needed for MacOS - Windows and Linux accidentally links zstd already.
-/// 
-/// This also doesn't work on Windows due to zstd not including a .pc file on Windows.
-func findMacOsZstdLinkerFlags() -> String {
-  let res =  readProcessOutput(
-    executable: findExecutableOnPath(name: "pkg-config")!, 
-    arguments: ["libzstd", "--libs-only-L"]) 
-  print("Linker flags: " + res) 
-  return res.trimmingCharacters(in: .whitespacesAndNewlines)
+/// Returns the flags for linking zstd on macOS.
+///
+/// LLVM requires zstd, which is a dependency, to be linked dynamically. This function returns the
+/// flags instructing the linker to do so.
+///
+/// This function does not work on Windows, as zstd does not include a pkg-config file on that
+/// platform. It is also not reliable on Linux yet because zstd generates wrong pkgconfig. TL;DR:
+/// libzstd.pc contains a non-architecture specific library directory on Linux.
+/// See https://github.com/facebook/zstd/issues/4488
+///
+/// The returned flaga are only needed on macOS. Windows and Linux link zstd already.
+func zstdLinkerFlagsForMacOS() -> String {
+  let r =  readProcessOutput(
+    executable: findExecutableOnPath(name: "pkg-config")!,
+    arguments: ["libzstd", "--libs-only-L"])
+  return r.trimmingCharacters(in: .whitespacesAndNewlines)
 }
 
 let llvmLinkerSettings =
   osIsWindows
   ? windowsLinkerSettings()
-  : [
-    .unsafeFlags([findMacOsZstdLinkerFlags()], .when(platforms: [.macOS]))
-  ]
+  : [.unsafeFlags([zstdLinkerFlagsForMacOS()], .when(platforms: [.macOS]))]
+
+// MARK: Package manifest
 
 let package = Package(
   name: "Swifty-LLVM",
