622cdaf811869fd2a0df36252345d4b875253bdb
Ambrus Toth <ping@ambrus.dev>
2026-05-22 00:06:39 +0200

    Fix and simplify llvmc dependency
    Using an updated .pc file that passes SPM-compatible library flags

--- Package.swift
+++ Package.swift
@@ -6,86 +6,13 @@ import Foundation
 import PackageDescription
 
 #if os(Windows)
-  let osIsWindows = true
+let osIsWindows = true
 #else
-  let osIsWindows = false
+let osIsWindows = false
 #endif
 
 /// The text used to separate elements of the PATH environment variable.
 let pathSeparator: Character = osIsWindows ? ";" : ":"
-
-/// Returns the contents of the pkg-config file for `package` if they can be found in
-/// `PKG_CONFIG_PATH`.
-///
-/// This function does not search the standard locations for the file as pkg-config would.
-func pkgConfigContents(_ package: String) -> String? {
-  if let p = ProcessInfo.processInfo.environment["PKG_CONFIG_PATH"] {
-    for q in p.split(separator: pathSeparator) {
-      if let s = try? String(contentsOfFile: "\(q)/\(package).pc", encoding: .utf8) { return s }
-    }
-  }
-  return nil
-}
-
-/// Returns the un-quoted, un-escaped elements in the remainder of the (logical) lines beginning
-/// with `"\(key): "` in `pcContents`, which are the contents of a pkg-config file.
-func pkgConfigValues(in pcContents: String, for key: String) throws -> [String] {
-  let keyPattern = NSRegularExpression.escapedPattern(for: key)
-  let lineHeadersPattern = #"(?m)(?<!\\[\r]?[\n])^[ \t]*"# + keyPattern + #"[ \t]*:[ \t]*"#
-  let lineHeaders = try pcContents.matches(forRegex: lineHeadersPattern).joined().compactMap { $0 }
-
-  var r: [String] = []
-  for h in lineHeaders {
-    var input = pcContents[h.endIndex...]
-
-    func add(_ c: Character) {
-      r[r.count - 1].append(c)
-    }
-
-    header: while let c = input.popFirst(), !c.isNewline {
-      switch c {
-      case "'", "\"":
-        let quote = c
-        quoting: while let c1 = input.popFirst() {
-          switch c1 {
-          case "\\":
-            if let c2 = input.popFirst() { add(c2) }
-          case quote:
-            break quoting
-          default: add(c1)
-          }
-        }
-      case "\\": if let c1 = input.popFirst() { add(c1) }
-      case _ where c.isNewline: break header
-      case _ where c.isWhitespace: break
-      case _: r[r.count - 1].append(c)
-      }
-    }
-  }
-  return r
-}
-
-extension String {
-
-  /// Returns, for each match of the valid regular expression `pattern`, the given match `groups`
-  /// in the same order in which they were passed.
-  func matches(forRegex pattern: String, groups: [Int] = [0]) throws -> [[Substring?]] {
-    let r = try NSRegularExpression(pattern: pattern)
-    return r.matches(in: self, range: NSRange(startIndex..<endIndex, in: self)).map { m in
-      groups.map { g in Range(m.range(at: g), in: self).map { self[ $0 ] } }
-    }
-  }
-
-}
-
-extension Substring {
-
-  /// Returns `prefix` if `self` is equal to `prefix + suffix`; otherwise, returns `self`.
-  func droppingSuffix(_ suffix: String) -> Substring {
-    self.hasSuffix(suffix) ? Substring(self.dropLast(suffix.count)) : self
-  }
-
-}
 
 extension Optional {
 
@@ -98,33 +25,23 @@ extension Optional {
 
 // MARK: Helpers for getting flags and other settings
 
-/// Returns the linker needed for building on Windows.
-func windowsLinkerSettings() -> [LinkerSetting] {
-  let t = pkgConfigContents("llvm") ??
-    fatalError("Didn't find llvm.pc in any of the locations of PKG_CONFIG_PATH.")
-
-  do {
-    let libs = try pkgConfigValues(in: t, for: "Libs")
-    return libs.compactMap { (lib) -> LinkerSetting? in
-      if !lib.starts(with: "-l") && (lib.first == "-") { return nil }
-
-      let rest = lib.dropFirst(lib.first == "-" ? 2 : 0)
-      let afterSlashes = rest.lastIndex(where: { $0 == "/" || $0 == "\\" })
-        .map { rest.index(after: $0) } ?? rest.startIndex
-
-      let s = rest[afterSlashes...].droppingSuffix(".lib")
-      return LinkerSetting.linkedLibrary(String(s))
-    }
-  } catch {
-    fatalError("Failed to acquire pkg-config values. Error: \(error)")
+/// Returns the value of the environment variable with given key.
+///
+/// On Windows, comparison is case-insensitive.
+func environmentEntry(forKey k: String) -> String? {
+  if osIsWindows {
+    ProcessInfo.processInfo.environment.first {
+      $0.key.caseInsensitiveCompare(k) == .orderedSame
+    }?.value
+  } else {
+    ProcessInfo.processInfo.environment[k]
   }
 }
 
 /// Returns the path to an executable named `name` or `name.exe` if one can be found at one of the
 /// locations in the PATH environment variable; otherwise, returns `nil`.
 func findExecutableOnPath(name: String) -> String? {
-  let path = ProcessInfo.processInfo.environment["PATH"] 
-    ?? fatalError("No PATH environment variable found")
+  let path = environmentEntry(forKey: "PATH") ?? fatalError("No PATH environment variable found")
 
   let executableFileName = osIsWindows ? "\(name).exe" : name
 
@@ -174,10 +91,10 @@ func readProcessOutput(executable: String, arguments: [String]) -> String? {
 /// The returned flags are only needed on macOS. Windows and Linux link zstd already.
 func zstdLinkerFlagsForMacOS() -> String {
   if let executable = findExecutableOnPath(name: "pkg-config"),
-    let ls = readProcessOutput(
-      executable: executable,
-      arguments: ["libzstd", "--libs-only-L"])
-  {
+  let ls = readProcessOutput(
+    executable: executable,
+    arguments: ["libzstd", "--libs-only-L"])
+    {
     return ls.trimmingCharacters(in: .whitespacesAndNewlines)
   }
 
@@ -189,18 +106,19 @@ func zstdLinkerFlagsForMacOS() -> String {
   ]
   for path in commonPaths {
     if FileManager.default.fileExists(atPath: "\(path)/libzstd.dylib")
-      || FileManager.default.fileExists(atPath: "\(path)/libzstd.a")
-    {
+    || FileManager.default.fileExists(atPath: "\(path)/libzstd.a")
+      {
       return "-L\(path)"
     }
   }
   fatalError("Failed to find zstd library both via pkg-config and common paths.")
 }
 
-let llvmLinkerSettings =
-  osIsWindows
-  ? windowsLinkerSettings()
-  : [.unsafeFlags([zstdLinkerFlagsForMacOS()], .when(platforms: [.macOS]))]
+#if os(macOS)
+let llvmLinkerSettings: [LinkerSetting] = [.unsafeFlags([zstdLinkerFlagsForMacOS()], .when(platforms: [.macOS]))]
+#else
+let llvmLinkerSettings: [LinkerSetting] = []
+#endif
 
 // MARK: Package manifest
 
